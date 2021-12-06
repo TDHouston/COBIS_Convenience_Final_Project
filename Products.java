39ddf9168401f6ec6d090c346c923f8c6973f8ba
TDHouston <89494245+TDHouston@users.noreply.github.com>
2021-12-06 02:32:59 -0500

    Create Products.java

---
+++ Products.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Products {
+    private String prod;
+    private double price;
+
+    public String getProd() {
+        return prod;
+    }
+
+    public void setProd(String prod) {
+        this.prod = prod;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
